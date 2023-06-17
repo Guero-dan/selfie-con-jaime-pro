de35506451adac4d78bfea5f4eccd9cf6f4dfbd2
Guero-dan <111529313+Guero-dan@users.noreply.github.com>
2023-06-16 19:46:17 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+amogus2=window.webkitSpeechRecognition;
+amogus3=new amogus2();
+amogus3.lang="es-MX";
+function iniciar(){
+    amogus3.start()
+}
+amogus3.onresult=function(amogus4){
+    console.log(amogus4)
+    amogus5=amogus4.results[0][0].transcript;
+    document.getElementById("textbox").value=amogus5;
+    if (amogus5.toLowerCase()=="amo"){
+        Webcam.attach("#camara");
+        hablarr("10,     9,     8,   7,       6,      5,      4,     3,     2,     1, di papta")
+        setTimeout(ijggggggggggggggggggggggggggggggggggggggggggggggggggggggdro6cjñrcjr, 10000)
+    }
+}
+function hablarr(msg){
+    var llerr=window.speechSynthesis;
+    var vez=new SpeechSynthesisUtterance(msg);
+    vez.lang="es-MX";
+    llerr.speak(vez);
+}
+Webcam.set({
+    width:360,
+    height:280,
+    image_format:"png",
+    png_quality:99
+})
+function ijggggggggggggggggggggggggggggggggggggggggggggggggggggggdro6cjñrcjr(){
+    Webcam.snap(function (dir){
+        document.getElementById("resultado").innerHTML='<img src="'+dir+'" id="amogus6">'
+        gn4iuiugnuhguiuw4hhwhutlsguizhhhihsrtgesuhuighguifhnjkrdngvfuhuidznjurkhjgdfgkdjghjnbdkjjruiejjkfnjkesrngjujrekongknfejgnvvvnejgnrngtbezshbgfdglbnjleslkjghljrhkjbkjhjhgrsbuyrhkjrskjbgrjbguhklesbutbubruingrjbresknjhr()
+    })
+}
+function gn4iuiugnuhguiuw4hhwhutlsguizhhhihsrtgesuhuighguifhnjkrdngvfuhuidznjurkhjgdfgkdjghjnbdkjjruiejjkfnjkesrngjujrekongknfejgnvvvnejgnrngtbezshbgfdglbnjleslkjghljrhkjbkjhjhgrsbuyrhkjrskjbgrjbguhklesbutbubruingrjbresknjhr(){
+foto=document.getElementById("amogus6").src;
+enlace =document.getElementById("link");
+enlace.href=foto;
+enlace.click()
+}
